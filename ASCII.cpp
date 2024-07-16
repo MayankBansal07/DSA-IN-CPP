41a264a5a83b4452e19000e9d7528ce7b673f51d
MayankBansal07 <143720231+MayankBansal07@users.noreply.github.com>
2024-07-16 15:51:45 +0530

    Delete ASCII.cpp

--- ASCII.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    // to print all ascii values with their alphabets
-    for(int i=65;i<=90;i++){
-        cout<<i<<" --> "<<char(i)<<endl;
-    }
-}
