ac808a7456ab264f8d36e1af2d31413b2e2ab9cc
Mayank Bansal <143720231+MayankBansal07@users.noreply.github.com>
2024-07-19 09:59:37 +0530

    Add files via upload

---
+++ Funcitons/Pascaltriangle_NcR.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+//pascal traingle using combination formula
+int fact(int x){
+     int f = 1;
+    for( int i=1;i<=x;i++){
+        f = f*i;
+    }
+    return f;
+}
+int ncr(int n,int r){
+    return fact(n)/(fact(r)*fact(n-r));
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for( int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<ncr(i,j)<<" ";
+        }
+        cout<<endl;
+    }
+}
