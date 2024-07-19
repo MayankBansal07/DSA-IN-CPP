95416a13ebb8cdb02bcc061d56a51de76bd138a3
Mayank Bansal <143720231+MayankBansal07@users.noreply.github.com>
2024-07-19 09:58:18 +0530

    Add files via upload

---
+++ Funcitons/library.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+// library functions using cmath
+int main(){
+    cout<<sqrt(2)<<endl; //square root
+    cout<<cbrt(10)<<endl; //cube root
+    cout<<min(4,8)<<endl;
+    cout<<max(4,8)<<endl;
+    cout<<pow(4,3)<<endl; //power
+}
