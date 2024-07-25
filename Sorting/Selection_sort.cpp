aeace1303eb1676cdfa6d630bc61664f2f79c5ee
Mayank Bansal <143720231+MayankBansal07@users.noreply.github.com>
2024-07-25 14:17:44 +0530

    Add files via upload

---
+++ Sorting/Selection_sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no. of elements of array: ";
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int index;
+    for (int i=0;i<n-1;i++)
+    {
+        index = i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[index])
+            index = j;
+        }
+        swap(arr[i],arr[index]);
+    }
+    cout<<"Array in increasing order: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
+
+
